6b5369784b31bdd530b08aa7e9602bde20cc05c5
cemtanrikut <cemtanrikut@outlook.com>
2022-02-15 20:45:06 +0300

    Configure struct and response header

--- api/pill/pill.go
+++ api/pill/pill.go
@@ -1,23 +1,30 @@
 package pill
 
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type pill struct {
-	name              string
-	barcode           string
-	atc_code          string
-	atc_name          string
-	company_name      string
-	prescription_type string
-	status            string
+	name              string `json:"name"`
+	barcode           string `json:"barcode"`
+	atc_code          string `json:"atc_code"`
+	atc_name          string `json:"atc_name"`
+	company_name      string `json:"company_name"`
+	prescription_type string `json:"prescription_type"`
+	status            string `json:"status"`
 }
 
-func getByName() {
+func getByName(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func getByBarcode(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
 
 }
 
-func getByBarcode() {
-
-}
-
-func getAll() {
+func getAll(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
 
 }
