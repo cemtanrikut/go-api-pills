f4cf10969fa6359dd304e1f31814b6049fceda3e
cemtanrikut <cemtanrikut@outlook.com>
2022-02-15 20:55:25 +0300

    Add get all pills func

--- api/pill/pill.go
+++ api/pill/pill.go
@@ -1,8 +1,12 @@
 package pill
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -13,7 +17,7 @@ type pill struct {
 	atc_name          string `json:"atc_name"`
 	company_name      string `json:"company_name"`
 	prescription_type string `json:"prescription_type"`
-	status            string `json:"status"`
+	status            bool   `json:"status"`
 }
 
 func getByName(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
@@ -22,9 +26,34 @@ func getByName(resp http.ResponseWriter, req *http.Request, collection *mongo.Co
 }
 
 func getByBarcode(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
-
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
-func getAll(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) {
+func getAll(resp http.ResponseWriter, req *http.Request, collection *mongo.Collection) ([]byte, error) {
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var pillMList []primitive.M
+
+	cursor, err := collection.Find(context.Background(), bson.M{"status": true})
+	if err != nil {
+		return nil, err
+	}
+
+	for cursor.Next(context.Background()) {
+		var pll bson.M
+		if err = cursor.Decode(&pll); err != nil {
+			return nil, err
+		}
+		pillMList = append(pillMList, pll)
+	}
+	defer cursor.Close(context.Background())
+
+	jsonResult, err := json.Marshal(pillMList)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonResult, nil
 
 }
