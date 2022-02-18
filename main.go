d9491770ab95a47006406e24e09a6613c2a74db0
cemtanrikut <cemtanrikut@outlook.com>
2022-02-18 20:43:55 +0300

    Add handler start

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/cemtanrikut/go-api-pills/client"
+)
+
+func main() {
+	log.Println("Starting the application")
+	client.MuxHandler()
 }
